0c32db2a4192fa6a0f7b818a922fdbc1f5b271da
carlosraul <carlosralvareza.es@gmail.com>
2026-02-23 12:26:43 +0100

    haciendo pull

--- src/Competicion.java
+++ src/Competicion.java
@@ -24,7 +24,11 @@ public class Competicion {
 
 
         //revision del pilot Nuevo que no exista en el array actual
+        for (int i = 0; i < pilotos.length; i++) {
+            pilotos[i]= 
 
+
+        }
 
 
 
